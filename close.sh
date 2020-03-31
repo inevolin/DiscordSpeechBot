8272b682cbc78c1b070c821720efacf0d9d44339
healzer <justdoitzlol@gmail.com>
2020-03-31 20:16:04 +0000

    close restart imrpoved

--- close.sh
+++ close.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kill -9 $(ps aux |grep discordspeechbot | awk '{print $2}')
+screen -SX discordspeechbot quit
 screen -wipe
--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-kill -9 $(ps aux |grep discordspeechbot | awk '{print $2}')
+screen -SX discordspeechbot quit
 screen -S discordspeechbot -dmL node index.js
 screen -wipe
