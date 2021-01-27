2ae90f1e6537e1c5c99686d7a936c5bab16d6fb0
healzer <justdoitzlol@gmail.com>
2021-01-27 21:43:56 +0000

    query casing

--- index.js
+++ index.js
@@ -437,7 +437,7 @@ function process_commands_query(query, mapKey, userid) {
     let out = null;
 
     const regex = /^music ([a-zA-Z]+)(.+?)?$/;
-    const m = query.match(regex);
+    const m = query.toLowerCase().match(regex);
     if (m && m.length) {
         const cmd = (m[1]||'').trim();
         const args = (m[2]||'').trim();
